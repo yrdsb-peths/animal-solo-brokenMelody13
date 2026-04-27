2bf608cc5aa9e7abcb8e2ba4d5f483a2fbee6af4
brokenMelody13 <440028996@gapps.yrdsb.ca>
2026-04-27 11:51:57 -0400

    made the elephont (the elepant's name) move,  then creat the food for elephont

--- MyWorld.java
+++ MyWorld.java
@@ -3,5 +3,11 @@ import greenfoot.*;
 public class MyWorld extends World {
     public MyWorld() {
         super(600, 400, 1);
+        
+        Elephant elephont = new Elephant();
+        addObject(elephont, 300, 200);
+        
+        Banana banana = new Banana();
+        addObject(banana, 300, 0);
     }
 }
