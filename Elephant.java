e44d6940de957b9c98f1aa950d63c92ffa3dea1c
brokenMelody13 <440028996@gapps.yrdsb.ca>
2026-04-28 22:28:57 -0400

    Change the speed of elephont's  trunk by adding a simple timer

--- Elephant.java
+++ Elephant.java
@@ -13,6 +13,7 @@ public class Elephant extends Actor
     GreenfootImage[] idleLeft = new GreenfootImage[8];
     // Direction the elephont is facing
     String facing = "right";
+    SimpleTimer animationTimer = new SimpleTimer();
     
     public Elephant()// The code gets run when object is created
     {
@@ -28,6 +29,10 @@ public class Elephant extends Actor
             idleLeft[i].mirrorHorizontally();
             idleLeft[i].scale(100, 100);
         }
+        
+        animationTimer.mark();
+        
+        
         setImage(idleRight[0]); // beginning image
     }
     
@@ -36,6 +41,12 @@ public class Elephant extends Actor
     int imageIndex = 0;
     public void animateElephant()
     {
+        if(animationTimer.millisElapsed() < 100)
+        {
+            return;
+        }
+        animationTimer.mark();
+        
         if(facing.equals("right"))
         {
             setImage(idleRight[imageIndex]);
