e4e0c0ffcb2d7b597f36a2ef7762e8b74b9e281d
brokenMelody13 <440028996@gapps.yrdsb.ca>
2026-04-28 21:36:41 -0400

    Add a background and sound effects

--- Elephant.java
+++ Elephant.java
@@ -8,10 +8,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Elephant extends Actor
 {
-    /**
-     * Act - do whatever the Elephant wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
+    GreenfootSound elephantSound = new GreenfootSound("elephantSound.mp3");
     
     
     public void act()
